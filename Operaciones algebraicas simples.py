695ac615525fe06d9efe57ee577ebb1495631c96
GironCicoira <85134299+GironCicoira@users.noreply.github.com>
2021-07-03 23:51:43 -0300

    Add files via upload

---
+++ Operaciones algebraicas simples.py
@@ -0,0 +1,25 @@
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+
+#suma
+print (a + b)
+
+#resta
+print (c - a)
+
+#miltiplicacion
+print (d * c)
+
+#exponente
+print (c**2)
+
+#division
+print (float(c) / a)
+
+#division enteros
+print (7 / 3)
+
+#modulo
+print (7%3)
